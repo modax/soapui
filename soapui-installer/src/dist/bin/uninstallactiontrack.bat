3c63b2419daf308d44049782bef9196357fe14e6
Modestas Vainius <modestas@vainius.eu>
2019-01-16 19:57:40 +0200

    Refactor bootstrap bat scripts to use common base
    This way all startup scripts contain consistent code. Before they were
    basically the same copy & pasted code with very minor changes (with
    exception of soapui.sh which had slightly more differences). But even
    they could be put into reusable base script.
    
    Base script is called baserunner.bat and it is "call"'ed from each
    bootstrap script. Improvements in comparison to prior code include:
    
    - JRE in SOAPUI_HOME support (1st priority) (previously only in dist_standalone);
    - JAVA_HOME support (2nd priority) (not in prior dist neither dist_standalone);
    - JAVA_OPTS from shell environment is passed to java process as well;
    - groovy.source.encoding=iso-8859-1 is not passed to JAVA_OPTS (used to
      be passed in dist_standalone but not in original dist).
    - bootstrap script no longer modify environment variables globally due
      to use of SETLOCAL
    
    Tested on Windows 7

--- soapui-installer/src/dist/bin/uninstallactiontrack.bat
+++ soapui-installer/src/dist/bin/uninstallactiontrack.bat
@@ -1,2 +1,3 @@
+SETLOCAL
 set CLASSPATH=..\lib\*
 ..\jre\bin\java -cp "%CLASSPATH%" TrackOutAppAction UninstallSoftware Product SoapUIOS Version %1 success true
--- soapui-installer/src/dist/bin/updateinstallation.bat
+++ soapui-installer/src/dist/bin/updateinstallation.bat
@@ -1,2 +1,3 @@
+SETLOCAL
 set CLASSPATH=..\lib\*
 ..\jre\bin\java -cp "%CLASSPATH%" TrackOutAppAction UpdateSoftware Product SoapUIOS Version %1 success true
